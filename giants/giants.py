b74cd6d20fdee1ad0396bbe22c6066d090705181
Nicholas Saunders <nksaunders1994@gmail.com>
2019-09-28 11:20:06 -1000

    specify output dir for plots

--- giants/giants.py
+++ giants/giants.py
@@ -136,7 +136,7 @@ class Giant(object):
                 data = eleanor.TargetData(star, height=15, width=15, bkg_size=31, do_psf=True, do_pca=True, try_load=True)
                 q = data.quality == 0
 
-                raw_lc = raw_lc.append(lk.LightCurve(time=data.time[q], flux=data.raw_flux[q], time_format='btjd'.remove_nans().normalize())
+                raw_lc = raw_lc.append(lk.LightCurve(time=data.time[q], flux=data.raw_flux[q], time_format='btjd').remove_nans().normalize())
                 corr_lc = corr_lc.append(lk.LightCurve(time=data.time[q], flux=data.corr_flux[q], time_format='btjd').remove_nans().normalize())
                 pca_lc = pca_lc.append(lk.LightCurve(time=data.time[q], flux=data.pca_flux[q], time_format='btjd').remove_nans().normalize())
                 psf_lc = psf_lc.append(lk.LightCurve(time=data.time[q], flux=data.psf_flux[q], time_format='btjd').remove_nans().normalize())
@@ -161,7 +161,7 @@ class Giant(object):
             lc = tpf.to_lightcurve(aperture_mask='threshold')
         return lc
 
-    def plot(self, ticid, use='eleanor'):
+    def plot(self, ticid, use='eleanor', outdir='plots'):
         """Produce a quick look plot to characterize giants in the TESS catalog.
 
         Parameters
@@ -170,6 +170,8 @@ class Giant(object):
             TIC ID of desired target
         use : "lightkurve" or "eleanor"
             Which package do you want to use to access the data?
+        outdir : str
+            Directory to save quick look plots into. Must be an existing directory.
 
         Saves
         -----
@@ -289,5 +291,5 @@ class Giant(object):
         fig = plt.gcf()
         fig.set_size_inches(12, 10)
 
-        fig.savefig('plots/'+str(ticid)+'_quicklook.png')
+        fig.savefig(outdir+'/'+str(ticid)+'_quicklook.png')
         plt.show()
