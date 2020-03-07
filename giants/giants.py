87c4f32c108fe8c730a58b70ed15de7b07364458
Nicholas Saunders <nksaunders1994@gmail.com>
2020-03-06 16:59:39 -1000

    fix4sarek

--- giants/giants.py
+++ giants/giants.py
@@ -480,6 +480,6 @@ class Giant(object):
 if __name__ == '__main__':
     try:
         target = Giant(csv_path='data/ticgiants_allsky_halo.csv')
-        target.plot(*sys.argv[1:])
+        target.plot(f'TIC {*sys.argv[1:]}')
     except:
         print(f'No data found for target {sys.argv[1]}')
