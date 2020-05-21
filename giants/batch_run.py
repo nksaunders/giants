c0525dca7fd9e886f77ae8692697f5625f92aded
Nicholas Saunders <nksaunders1994@gmail.com>
2020-05-20 18:26:26 -1000

    batch fits script

---
+++ giants/batch_run.py
@@ -0,0 +1,43 @@
+"""
+Script to run a batch of TIC IDs.
+"""
+
+
+try:
+    from .giants import Giant
+except:
+    from giants import Giant
+
+
+def save_fits_file(ticid):
+    try:
+        target = Giant(ticid=ticid, csv_path='data/ticgiants_allsky_halo.csv',
+                         cache_path='/data/sarek1/nksaun/lightkurve_cache')
+
+        target.save_to_fits(outdir='/data/sarek1/nksaun/tess_giants/zinn_lcs')
+
+    except:
+        try:
+            target = Giant(ticid=ticid, csv_path='data/ticgiants_allsky_halo.csv',
+                             cache_path='/data/sarek1/nksaun/lightkurve_cache')
+
+            target.save_to_fits(outdir='/data/sarek1/nksaun/tess_giants/zinn_lcs')
+
+        except:
+            try:
+                target = Giant(ticid=ticid, csv_path='data/ticgiants_allsky_halo.csv',
+                                 cache_path='/data/sarek1/nksaun/lightkurve_cache')
+
+                target.save_to_fits(outdir='/data/sarek1/nksaun/tess_giants/zinn_lcs')
+            except:
+                pass
+
+
+if __name__ == '__main__':
+    try:
+        ticid = sys.argv[1]
+        print(f'Generating fits files for target {ticid}...')
+
+        save_fits_file(ticid)
+    except:
+        print(f'Failed to save fits files for target {ticid}.')
