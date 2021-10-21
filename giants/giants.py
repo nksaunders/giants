8ad497fb7dfa08091457a2b4392092ee76a12708
Nicholas Saunders <nksaunders1994@gmail.com>
2021-10-21 14:04:30 -0400

    remove eleanor dependency

--- giants/giants.py
+++ giants/giants.py
@@ -13,7 +13,7 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import astropy.units as u
 # import ktransit
 from matplotlib.backends.backend_pdf import PdfPages
-import eleanor
+# import eleanor
 try:
     from . import lomb
     from . import PACKAGEDIR
