aed00f9b98699f7eb4c0832b1c451875fb65803f
Nicholas Saunders <nksaunders1994@gmail.com>
2022-02-15 17:31:08 -1000

    add giants run script

---
+++ src/giants/run_giants.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from .plotting import plot_summary
+from .target import Target
+
+
+if __name__ == '__main__':
+    try:
+        ticid = sys.argv[1]
+        outdir = sys.argv[2]
+        output = "plot"
+        try:
+            output = sys.argv[3]
+        except:
+            pass
+
+        target = Target(ticid=ticid)
+
+        if output=="plot":
+            target.fetch_and_clean_data(lc_source='local')
+            plot_summary(target, outdir=outdir, save_data=True)
+        else:
+            target.save_to_fits(outdir=outdir)
+    except:
+        print(f'Target {sys.argv[1]} failed.')
