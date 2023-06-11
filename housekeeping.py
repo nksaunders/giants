84a5e062f921f2d15ba0c806066bed88245f63bd
Nicholas Saunders <nksaunders1994@gmail.com>
2023-06-11 12:10:25 -1000

    idiot (me) proof the script

--- housekeeping.py
+++ housekeeping.py
@@ -6,10 +6,10 @@ dirpath = '/home/nsaunders/.lightkurve-cache/tesscut/'
 while True:
     time.sleep(60)
     for f in os.listdir(dirpath):
-        fn = os.path.join(dirpath, f)
-        if os.stat(fn).st_mtime < time.time() - 60:
-            try:
+        try:
+            fn = os.path.join(dirpath, f)
+            if os.stat(fn).st_mtime < time.time() - 60:
                 os.remove(fn)
                 print(f'Removed {f}')
-            except:
-                print(f'Could not remove {f}')
+        except:
+            print(f'Could not remove {f}')
