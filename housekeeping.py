22f16e5af24531d1934820f431a25a41100415d0
Nicholas Saunders <nksaunders1994@gmail.com>
2023-06-11 11:53:50 -1000

    find correct file

--- housekeeping.py
+++ housekeeping.py
@@ -6,9 +6,10 @@ dirpath = '/home/nsaunders/.lightkurve-cache/tesscut/'
 while True:
     time.sleep(60)
     for f in os.listdir(dirpath):
-        if os.stat(f).st_mtime < time.time() - 60:
+        fn = os.path.join(dirpath, f)
+        if os.stat(fn).st_mtime < time.time() - 60:
             try:
-                os.remove(os.path.join(dirpath, f))
+                os.remove(fn)
                 print(f'Removed {f}')
             except:
                 print(f'Could not remove {f}')
